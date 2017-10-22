f7680db5ab8f1f533159ae8e68b15e3909c41f27
Chris Swinchatt <C.Swinchatt@Sussex.ac.uk>
2017-10-22 19:37:32 +0100

    Create basic Node.js webserver

---
+++ server/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const http = require('http');
+
+const PORT = 80;
+
+module.exports.startServer = function(port) {
+    const requestHandler = function(request, response) {
+        console.log('Request: <address=' + request.connection.remoteAddress + ', url=' + request.url + '>');
+        response.end('Success!');
+    }
+    const server = http.createServer(requestHandler);
+    server.listen(port, function(err) {
+        if (err) {
+            return console.log('Error: ' + err);
+        }
+        console.log('Listening on localhost:' + port);
+    });
+    return server;
+}
