fe92b1a919ba5f55af3ecc3df6c2f28bccda6d5a
Chris Swinchatt <C.Swinchatt@Sussex.ac.uk>
2017-10-25 13:36:12 +0100

    Implement persistence

--- server/src/server/config.js
+++ server/src/server/config.js
@@ -14,5 +14,5 @@ module.exports.defaults = {
     LOGLEVEL: process.env.LOGLEVEL || log.LogLevel.DEBUG,
     ADDRESS:  process.env.ADDRESS  || '127.0.0.1',
     PORT:     process.env.PORT     || 80,
-    DATABASE: process.env.DATABASE || 'database.json'
+    DATABASE: process.env.DATABASE || 'server.db'
 };
