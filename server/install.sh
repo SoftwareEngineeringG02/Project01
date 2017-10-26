e7571a83e87c52769bd5bf27b6a6a5120016e463
Chris Swinchatt <C.Swinchatt@Sussex.ac.uk>
2017-10-26 11:51:09 +0100

    Make install script pull the full repository
    We can now install the server with:
    $ curl "https://raw.githubusercontent.com/SoftwareEngineeringG02/Project01/chris-server/server/install.sh" | /bin/bash

--- server/install.sh
+++ server/install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 #
-# Server installer for
+# Server installation script.
 #
-
-set -e
 
 program_name=$(basename "$0")
 nvm_git="https://github.com/creationix/nvm.git"
 node_version="8.7.0"
+github_url="https://github.com"
+project_name="SoftwareEngineeringG02/Project01"
+project_branch="chris-server"
+project_git="${github_url}/${project_name}.git"
+project_dir=$(basename "${project_name}")
+server_dir="server"
 
 function say {
     echo "${program_name}: *** $@"
@@ -25,9 +29,19 @@ function check_env {
     [[ -x $(which git) ]] || die "Git is required but it was not found"
 }
 
+# Download the server from Github.
+function get_server {
+    git clone "${project_git}"
+    [[ -d "${project_dir}" ]] || die "Failed to clone Git repository: No directory ${project_dir}"
+    cd "${project_dir}"
+    git checkout "${project_branch}"
+    [[ -d "${server_dir}" ]] || die "Failed to clone Git repository: No directory ${server_dir}"
+    cd "${server_dir}"
+}
+
 # Install Node.js if required
 function install_node {
-    if [[ -x $(which node) && "x$(node --version)" = "x${node_version}" ]]; then
+    if [[ -x $(which node) && "x$(node --version)" = "xv${node_version}" ]]; then
         say "Correct version of node is already installed"
     else
         say "Node is not installed or is not the right version, installing version ${node_version}"
@@ -36,11 +50,11 @@ function install_node {
         pushd nvm
         ./install.sh
         popd
-        # Source .bashrc to get NVM in the path
-        . ~/.bashrc
-        # Install Node with NVM
-        nvm install "${node_version}"
-        nvm use "${node_version}"
+        # Install Node with NVM.
+        export NVM_DIR="$HOME/.nvm"
+        . "$NVM_DIR/nvm.sh"
+        . "$NVM_DIR/bash_completion"
+        nvm install ${node_version}
     fi
 }
 
@@ -48,9 +62,10 @@ function install_node {
 function install_deps {
     # Get dependencies with NPM
     npm install
-    say "Installation complete; say './launch-server' to start server"
+    say "Installation complete; say '${project_dir}/${server_dir}/launch-server' to start server"
 }
 
 check_env
+get_server
 install_node
 install_deps
