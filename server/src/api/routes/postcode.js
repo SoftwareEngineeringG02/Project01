5faf51a98250bdcb95ac423bd838141b5918b40d
Matt Nottage <matt.nottage@jellyfish.co.uk>
2017-11-23 11:24:42 +0000

    Codacy clean-up for postcodes.js

--- server/src/api/routes/postcode.js
+++ server/src/api/routes/postcode.js
@@ -1,17 +1,17 @@
 /**
  * @module api/routes/postcode
  */
-const http = require('http');
+const http = require("http");
 
 const controller = require(`${SERVER_ROOT}/api/controller`);
 const log        = require(`${SERVER_ROOT}/server/log`);
 const model      = require(`${SERVER_ROOT}/api/model`);
 const routes     = require(`${SERVER_ROOT}/api/routes`);
-const util       = require(`${SERVER_ROOT}/util`)
+const util       = require(`${SERVER_ROOT}/util`);
 
-module.exports.REL    = 'get-postcode'
+module.exports.REL    = "get-postcode";
 
-module.exports.METHOD = 'POST';
+module.exports.METHOD = "POST";
 
 module.exports.CALLBACK = function(request, response) {
     log.debug(module.exports.REL);
@@ -20,18 +20,18 @@ module.exports.CALLBACK = function(request, response) {
 
 function handleBody(request, response, error, body) {
     if (error) {
-        return request.emit('error', error);
+        return request.emit("error", error);
     }
     util.getJsonElements(
         body,
-        {'id': 'string', 'time': 'number', 'longitude': 'number', 'latitude': 'number'},
+        {"id": "string", "time": "number", "longitude": "number", "latitude": "number"},
         handleJson.bind(null, request, response)
     );
 }
 
 function handleJson(request, response, error, object) {
     if (error) {
-        return request.emit('error', error);
+        return request.emit("error", error);
     }
     const { id, time, longitude, latitude } = object;
     model.startRequest(
@@ -44,7 +44,7 @@ function handleJson(request, response, error, object) {
 
 function getPostcode(request, response, longitude, latitude, error, requestID) {
     if (error) {
-        return request.emit('error', error);
+        return request.emit("error", error);
     }
     model.getPostcode(
         longitude,
@@ -55,16 +55,16 @@ function getPostcode(request, response, longitude, latitude, error, requestID) {
 
 function handlePostcode(request, response, requestID, error, postcode) {
     if (error) {
-        return request.emit('error', error);
+        return request.emit("error", error);
     }
     // Send response.
     controller.doResponse(
         response,
         {
-            'error':    0,
-            'message':  'Success',
-            'postcode': postcode,
-            'links':    routes.endpoints
+            "error":    0,
+            "message":  "Success",
+            "postcode": postcode,
+            "links":    routes.endpoints
         },
         200,
         requestID
