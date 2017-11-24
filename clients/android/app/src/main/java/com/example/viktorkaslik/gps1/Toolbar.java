0d4727cbce8b358dbc4aa737b09c6297028e7fee
Viktor <vk86@sussex.ac.uk>
2017-11-24 08:50:36 +0000

    Fixed SDK git issue & merged viktors updates

---
+++ clients/android/app/src/main/java/com/example/viktorkaslik/gps1/Toolbar.java
@@ -0,0 +1,13 @@
+package com.example.viktorkaslik.gps1;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by viktorkaslik on 06/11/2017.
+ */
+
+public class Toolbar extends AppCompatActivity {
+
+
+
+}
